f5deef240e5bd506f64f9a67c3d8174b6eb1ce6b
tygrine <tygrine@gmail.com>
2020-03-19 11:40:27 +0800

    deploy

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,32 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
-require('./forum.blade');
-require('./show.blade');
 
 window.Vue = require('vue');
 
-Vue.component('notification', require('./components/Notification.vue').default);
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
 const app = new Vue({
     el: '#app',
-    data: {
-        notifications: '',
-    },
-    created() {
-        axios.post('/notification/get').then(response => {
-            $data = this.notifications = response.data;
-            $like = $data.filter(data=> data.like === 'true');
-        });
-
-        var userid = $('meta[name="userid"]').attr('content');
-        Echo.private('App.User.' + userid).notification((notification) => {
-            this.notifications.push(notification)
-        });
-    },
 });
